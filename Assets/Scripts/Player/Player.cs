79910bbcb790d218653a32e2d3d6bfd36682e7f2
Deep June <wjdwnsrn9021@naver.com>
2022-07-18 01:12:33 +0900

    animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : LivingEntity {
+    RaycastHit hit;
+    [SerializeField] LayerMask layer;
     public Rigidbody playerRigidbody;
     protected StateMachine playerStateMachine;
     [SerializeField] protected Animator playerAnimator;
     [SerializeField] GameObject playerAvatar;
     Vector3 currentSpeed;
-
     float moveSpeed = 8f;
     float dodgePower = 35f;
-    bool isDodge = false;
+    protected bool canMove = true;
 
     protected State idleState = new State("idle");
     protected State moveState = new State("move");
     protected State dodgeState = new State("dodge");
-
+    protected State abilityState = new State("ability");
     protected void Start() {
         playerStateMachine = GetComponent<StateMachine>();
         InitializeState();
@@ -36,16 +37,22 @@ public class Player : LivingEntity {
             playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
         };
         dodgeState.OnActive += () =>{
-            isDodge = true;
+            canMove = false;
             Vector3 playerAngle = playerAvatar.transform.forward;
             StartCoroutine(DodgeCoroutine());
             playerAnimator.SetBool("Dodge", true);
             playerRigidbody.AddForce(playerAngle * dodgePower, ForceMode.Impulse);
         };
         dodgeState.OnInactive += () => {
-            isDodge = false;
+            canMove = true;
             playerAnimator.SetBool("Dodge", false);
             playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
+        };
+        abilityState.OnActive += () => {
+
+        };
+        abilityState.OnInactive += () => {
+            
         };
     }
     // Weapon
@@ -53,18 +60,18 @@ public class Player : LivingEntity {
     virtual public void SpecialAttack() {}
     virtual public void UtilityAbility() {}
     // (Q)skill
-    void PlayerAbility() {
-
+    public void PlayerAbility() {
+        playerStateMachine.ChangeState(abilityState);
     }
     public void Idle() {
-        if(!isDodge) {
+        if(canMove) {
             this.currentSpeed = Vector3.zero;
             playerStateMachine.ChangeState(idleState);
         }
     }
     // Player Move
     public void Move(Vector3 direction)  {
-        if(!isDodge) {
+        if(canMove) {
             this.currentSpeed = direction.normalized * moveSpeed;
             playerStateMachine.ChangeState(moveState);
         }
@@ -75,5 +82,15 @@ public class Player : LivingEntity {
     IEnumerator DodgeCoroutine(){
          yield return new WaitForSeconds(.3f);
         playerStateMachine.ChangeState(idleState);
+    }
+    void FrontCheck() {
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, look * 1, Color.red); 
+        if(Physics.Raycast(transform.position + Vector3.up, look, out hit, 1, layer)) {
+            Debug.Log("Raycast!");
+        }
+    }
+    void Update() {
+        FrontCheck();
     }
 }
---
+++ Assets/Scripts/Player/ScythePlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScythePlayer : Player
+{
+    new void Start()
+    {
+        base.Start();
+    }
+}
